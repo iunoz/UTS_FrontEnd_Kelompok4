3426acf0bceb11e545519fe92815acc6b70ec85a
iunoz <gregorius.535230114@stu.untar.ac.id>
2024-09-29 15:56:43 +0700

    page about (add funfact and pie chart without animation, then add animation in team section) and tidy up structure

---
+++ JS/about.js
@@ -0,0 +1,26 @@
+//About Team
+var typed1 = new Typed("#name1", {
+    strings: ["Melvyn", "Benaya", "Wijaya"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    showCursor: false
+});
+
+var typed2 = new Typed("#name2", {
+    strings: ["Gregorius", "Nathanyel", "Benedict"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    showCursor: false
+});
+
+var typed3 = new Typed("#name3", {
+    strings: ["Ryan", "Hermanto"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    showCursor: false
+});
+
+
