b52c4894cdb7bdcbaba1745e18710590cc702dde
iunoz <gregorius.535230114@stu.untar.ac.id>
2024-10-09 17:16:10 +0700

    Added search funtion for shop (without css)

---
+++ JS/search.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#search").on("keyup", function() { //ketrigger setiap mengetik di input pencarian (keyup)
+        var value = $(this).val().toLowerCase(); //ambil nilai input dan ubah menjadi lowercase
+        //filter produk berdasarkan input pencarian
+        $(".product-item").filter(function() {
+            //menampilkan produk jika namanya mengandung pada teks pencarian sbelumnya
+            $(this).toggle($(this).find("h3").text().toLowerCase().indexOf(value) > -1);
+        });
+    });
+});
